417401b1ad5229dbc0d00d31eb9ab73ea63c81e0
Connor Glynn <connor.glynn@digital.justice.gov.uk>
2022-03-16 20:12:38 +0000

    DCS-1501: ✨Add prompts to optionally select scenarios and mode on the fly

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const prompts = require('prompts');
 const config = require('./lib/utils/config');
 const { BasmApi } = require('./lib/clients/basmApi');
 const { BasmService } = require('./lib/service/basmService');
@@ -6,23 +7,46 @@ const { logger } = require('./lib/utils/logger');
 const Generator = require('./lib/generators');
 const printReferenceData = require('./lib/referenceData');
 
-const run = async (mode = 'GENERATE') => {
+const ModeEnum = { GENERATE: 'GENERATE', PRINT_REFERENCE_DATA: 'PRINT_REFERENCE_DATA' };
+const ScenariosEnum = { NO_IDENTIFIERS: 'NO_IDENTIFIERS', PNC_ONLY: 'PNC_ONLY', FROM_CUSTODY_SUITE: 'FROM_CUSTODY_SUITE' };
+
+const multiselectPrompt = async (message = 'message', choices = [], max = 99, hint = '- Space to select. Return to submit') => prompts({
+  type: 'multiselect',
+  name: 'value',
+  message,
+  choices,
+  max,
+  hint,
+}).then((result) => result.value);
+
+const run = async () => {
   const basmApi = new BasmApi(new JsonApiClient(logger.noop, config.apiEndpoint));
   const basmService = new BasmService(logger.console, basmApi);
   await basmService.initToken();
   const generator = new Generator(logger.console, basmService);
 
+  const mode = await multiselectPrompt('Running mode', [
+    { title: 'Generate Test Data', value: ModeEnum.GENERATE },
+    { title: 'Print BaSM Reference Data', value: ModeEnum.PRINT_REFERENCE_DATA },
+  ], 1).then((result) => result[0]);
+
   switch (mode) {
-    case 'GENERATE': {
-      await generator.fromCourt();
+    case ModeEnum.GENERATE: {
+      const scenarios = await multiselectPrompt('Choose Scenarios', [
+        { title: 'No Identifiers', value: ScenariosEnum.NO_IDENTIFIERS },
+        { title: 'PNC Only', value: ScenariosEnum.PNC_ONLY },
+        { title: 'From Custody Suite', value: ScenariosEnum.FROM_CUSTODY_SUITE },
+      ]);
 
-      await generator.fromCourtWithPnc();
+      if (scenarios.includes(ScenariosEnum.NO_IDENTIFIERS)) await generator.fromCourt();
+      if (scenarios.includes(ScenariosEnum.PNC_ONLY)) await generator.fromCourtWithPnc();
+      // eslint-disable-next-line max-len
+      if (scenarios.includes(ScenariosEnum.FROM_CUSTODY_SUITE)) await generator.fromPoliceCustodySuite();
 
-      await generator.fromPoliceCustodySuite();
       return;
     }
-    case 'PRINT_REFERENCE_DATA': {
-      printReferenceData(logger.console, basmApi);
+    case ModeEnum.PRINT_REFERENCE_DATA: {
+      await printReferenceData(logger.console, basmApi);
       break;
     }
     default:
