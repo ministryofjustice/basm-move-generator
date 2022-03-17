e82367a1c4c493339469f78b7f79f7db40f39b59
Connor Glynn <connor.glynn@digital.justice.gov.uk>
2022-03-17 15:05:12 +0000

    DCS-1501: ♻️ some refactoring

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-const prompts = require('prompts');
 const config = require('./lib/utils/config');
 const { BasmApi } = require('./lib/clients/basmApi');
 const { BasmService } = require('./lib/service/basmService');
 const { JsonApiClient } = require('./lib/clients/jsonApiClient');
 const { logger } = require('./lib/utils/logger');
 const Generator = require('./lib/generators');
-const printReferenceData = require('./lib/referenceData');
-const { questions, ModeEnum, ScenariosEnum } = require('./lib/utils/questions');
+const wpipPrompt = require('./lib/prompts/wpipPrompt');
 
 const run = async () => {
   const basmApi = new BasmApi(new JsonApiClient(logger.noop, config.apiEndpoint));
@@ -14,17 +12,7 @@ const run = async () => {
   await basmService.initToken();
   const generator = new Generator(logger.console, basmService);
 
-  const { mode, scenarios } = await prompts(questions);
-
-  if (mode === ModeEnum.GENERATE) {
-    if (scenarios.includes(ScenariosEnum.NO_IDENTIFIERS)) await generator.fromCourt();
-    if (scenarios.includes(ScenariosEnum.PNC_ONLY)) await generator.fromCourtWithPnc();
-    if (scenarios.includes(ScenariosEnum.FROM_CUSTODY_SUITE)) await generator.fromPoliceCustodySuite();
-  } else if (mode === ModeEnum.PRINT_REFERENCE_DATA) {
-    await printReferenceData(logger.console, basmApi);
-  } else {
-    throw Error('Data not generated or printed');
-  }
+  await wpipPrompt(generator, basmApi);
 };
 
 run().catch((e) => {
