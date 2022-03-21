da8e4c58ada14d1493d7dbf4bbc6e4399d8ae34d
Connor Glynn <66882795+connormaglynn@users.noreply.github.com>
2022-03-21 16:56:48 +0000

    DCS-1501: ✨Add prompts to optionally select scenarios and mode on the fly (#22)

--- index.js
+++ index.js
@@ -4,30 +4,15 @@ const { BasmService } = require('./lib/service/basmService');
 const { JsonApiClient } = require('./lib/clients/jsonApiClient');
 const { logger } = require('./lib/utils/logger');
 const Generator = require('./lib/generators');
-const printReferenceData = require('./lib/referenceData');
+const wpipPrompt = require('./lib/prompts/wpipPrompt');
 
-const run = async (mode = 'GENERATE') => {
+const run = async () => {
   const basmApi = new BasmApi(new JsonApiClient(logger.noop, config.apiEndpoint));
   const basmService = new BasmService(logger.console, basmApi);
   await basmService.initToken();
   const generator = new Generator(logger.console, basmService);
 
-  switch (mode) {
-    case 'GENERATE': {
-      await generator.fromCourt();
-
-      await generator.fromCourtWithPnc();
-
-      await generator.fromPoliceCustodySuite();
-      return;
-    }
-    case 'PRINT_REFERENCE_DATA': {
-      printReferenceData(logger.console, basmApi);
-      break;
-    }
-    default:
-      throw Error('Data not generated or printed');
-  }
+  await wpipPrompt(generator, basmApi);
 };
 
 run().catch((e) => {
