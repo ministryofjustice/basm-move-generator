cd674b2d4687faa212ea296db3ff01caa786b69e
connormaglynn <connor.glynn@digital.justice.gov.uk>
2022-03-28 17:59:35 +0100

    DCS-1502: ✨ Add Unmatched PNC scenario with Auth and PrisonerSearchClient ♻️ Refactor FromCourtWithPNC to CourtReturn

--- index.js
+++ index.js
@@ -5,17 +5,25 @@ const { JsonApiClient } = require('./lib/clients/jsonApiClient');
 const { logger } = require('./lib/utils/logger');
 const Generator = require('./lib/generators');
 const wpipPrompt = require('./lib/prompts/wpipPrompt');
+const { AuthClient } = require('./lib/clients/authClient');
+const { PrisonerSearchService } = require('./lib/service/prisonerSearchService');
+const { PrisonerSearchClient } = require('./lib/clients/prisonerSearchClient');
 
 const run = async () => {
-  const basmApi = new BasmApi(new JsonApiClient(logger.noop, config.apiEndpoint));
+  const authClient = new AuthClient(logger.console, config.auth.apiEndpoint);
+  const prisonerSearchClient = new PrisonerSearchClient(logger.console, config.prisonerSearch.apiEndpoint, authClient);
+  const prisonerSearchService = new PrisonerSearchService(logger.console, prisonerSearchClient);
+
+  const basmApi = new BasmApi(new JsonApiClient(logger.noop, config.basm.apiEndpoint));
   const basmService = new BasmService(logger.console, basmApi);
   await basmService.initToken();
-  const generator = new Generator(logger.console, basmService);
+
+  const generator = new Generator(logger.console, basmService, prisonerSearchService);
 
   await wpipPrompt(generator, basmApi);
 };
 
-run().catch((e) => {
+run().catch(e => {
   console.error('!!!  ERROR  !!!');
   console.error(e.message);
   console.error(e?.config?.url);
