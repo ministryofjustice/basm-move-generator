13240921504562dbfa7e59c6143e1fa40742469d
dafhoustonmoj <dafydd.houston@justice.gov.uk>
2025-09-26 14:40:41 +0100

    MAP-2816: upgrade lint and fix errors and wanrings

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
-const config = require('./lib/utils/config');
-const { BasmApi } = require('./lib/clients/basmApi');
-const { BasmService } = require('./lib/service/basmService');
-const { JsonApiClient } = require('./lib/clients/jsonApiClient');
-const { logger } = require('./lib/utils/logger');
-const Generator = require('./lib/generators');
-const wpipPrompt = require('./lib/prompts/wpipPrompt');
-const { AuthClient } = require('./lib/clients/authClient');
-const { PrisonerSearchService } = require('./lib/service/prisonerSearchService');
-const { PrisonerSearchClient } = require('./lib/clients/prisonerSearchClient');
+const config = require('./lib/utils/config')
+const { BasmApi } = require('./lib/clients/basmApi')
+const { BasmService } = require('./lib/service/basmService')
+const { JsonApiClient } = require('./lib/clients/jsonApiClient')
+const { logger } = require('./lib/utils/logger')
+const Generator = require('./lib/generators')
+const wpipPrompt = require('./lib/prompts/wpipPrompt')
+const { AuthClient } = require('./lib/clients/authClient')
+const { PrisonerSearchService } = require('./lib/service/prisonerSearchService')
+const { PrisonerSearchClient } = require('./lib/clients/prisonerSearchClient')
 
 const run = async () => {
-  const authClient = new AuthClient(logger.console, config.auth.apiEndpoint);
-  const prisonerSearchClient = new PrisonerSearchClient(logger.console, config.prisonerSearch.apiEndpoint, authClient);
-  const prisonerSearchService = new PrisonerSearchService(logger.console, prisonerSearchClient);
+  const authClient = new AuthClient(logger.console, config.auth.apiEndpoint)
+  const prisonerSearchClient = new PrisonerSearchClient(logger.console, config.prisonerSearch.apiEndpoint, authClient)
+  const prisonerSearchService = new PrisonerSearchService(logger.console, prisonerSearchClient)
 
-  const basmApi = new BasmApi(new JsonApiClient(logger.noop, config.basm.apiEndpoint));
-  const basmService = new BasmService(logger.console, basmApi, config.basm.toPrison);
-  await basmService.initToken();
+  const basmApi = new BasmApi(new JsonApiClient(logger.noop, config.basm.apiEndpoint))
+  const basmService = new BasmService(logger.console, basmApi, config.basm.toPrison)
+  await basmService.initToken()
 
-  const generator = new Generator(logger.console, basmService, prisonerSearchService);
+  const generator = new Generator(logger.console, basmService, prisonerSearchService)
 
-  await wpipPrompt(generator, basmApi);
-};
+  await wpipPrompt(generator, basmApi)
+}
 
 run().catch(e => {
-  console.error('!!!  ERROR  !!!');
-  console.error(e.message);
-  console.error(e?.config?.url);
-  console.error(e?.config?.data);
-  console.error(JSON.stringify(e?.response?.data, null, 2));
-  console.error(e);
-});
+  const url = e.config && e.config.url
+  const data = e.data && e.config.data
+  console.error('!!!  ERROR  !!!')
+  console.error(e.message)
+  console.error(url)
+  console.error(data)
+  console.error(JSON.stringify(data, null, 2))
+  console.error(e)
+})
